67002553ce32ff8cb3099e57f21179d7605bf8c7
kelvinjoshua <kelvinbale78@gmail.com>
2021-05-27 16:36:01 +0300

    Butterknife dependency and implementation

--- app/src/main/java/com/example/greener/SignIn.java
+++ app/src/main/java/com/example/greener/SignIn.java
@@ -2,13 +2,34 @@ package com.example.greener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class SignIn extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class SignIn extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.Login) Button loginButton;
+    @BindView(R.id.createText) TextView createText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        ButterKnife.bind(this);
+        loginButton.setOnClickListener(this);
+        createText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == createText){
+            Intent intent = new Intent(SignIn.this, CreateAcc.class);
+            startActivity(intent);
+        }
+
     }
 }
