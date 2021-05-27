b71e9c5eccfe98fde9a0c818f5b79e336842e7cf
kelvinjoshua <kelvinbale78@gmail.com>
2021-05-27 15:18:01 +0300

    Sign in UI and assets

---
+++ app/src/main/java/com/example/greener/CreateAcc.java
@@ -0,0 +1,13 @@
+package com.example.greener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAcc extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_acc);
+    }
+}
