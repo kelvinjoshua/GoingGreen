58a767233747a6a33c30fa3bb2b18dc67bbca4bc
kelvinjoshua <kelvinbale78@gmail.com>
2021-05-27 17:56:40 +0300

    Load delay

--- app/src/main/java/com/example/greener/Splash.java
+++ app/src/main/java/com/example/greener/Splash.java
@@ -2,13 +2,26 @@ package com.example.greener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
+
+    private static int Load_delay = 3500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, SignIn.class);
+                startActivity(intent);
+                finish();// called when activity is done and should be closed
+            }
+        },Load_delay);
     }
 }
